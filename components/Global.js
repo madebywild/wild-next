143cd84eaeb2268985c4f8471e0dfc24751002a6
Moriz Büsing <moriz.buesing@gmail.com>
2019-02-04 17:32:29 +0100

    fix global styling

--- components/Global.js
+++ components/Global.js
@@ -3,7 +3,6 @@ import styles from "./Global.scss";
 
 export default props => (
   <div>
-    <div className="test">Test</div>
     {props.children}
     <style jsx global>{styles}</style>
   </div>
