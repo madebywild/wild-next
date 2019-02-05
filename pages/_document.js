9ca9feeda1cb08884da52523aba83cc00b1b23f8
Moriz Büsing <moriz.buesing@gmail.com>
2019-02-05 19:08:25 +0100

    rename breakpoints

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,16 @@ export default class MyDocument extends Document {
       <html lang="en">
         <Head>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <link rel="shortcut icon" href="/static/favicon.ico" />
+
+          <link rel="apple-touch-icon" sizes="180x180" href="./static/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="./static/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="./static/favicon/favicon-16x16.png" />
+          <link rel="mask-icon" href="./static/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="shortcut icon" href="./static/favicon/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-config" content="./static/favicon/browserconfig.xml" />
+          <meta name="theme-color" content="#ffffff" />
+
           <script dangerouslySetInnerHTML={{__html: envScript}} />
         </Head>
         <body>
