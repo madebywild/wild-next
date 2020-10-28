6249dd0d0777edfeab822405271a0eebcda91bd3
Oskar Rainer <rainer.oskar@gmail.com>
2020-10-28 10:04:00 +0100

    fix: remove legacy fixes

--- next.config.js
+++ next.config.js
@@ -3,17 +3,9 @@ const withReactSvg = require("next-react-svg");
 
 let config = {};
 
-config = {
-  // Set root directory for `next-react-svg`.
+// Set root directory for `next-react-svg`.
+config = withReactSvg({
   include: path.resolve(__dirname, "src/assets"),
-
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module.
-    if (!isServer) config.node = { fs: "empty" };
-    return config;
-  },
-};
-
-config = withReactSvg(config);
+});
 
 module.exports = config;
