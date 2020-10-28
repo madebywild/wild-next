0032f1e156de8c41d97baa98f5a0307d268c2760
Oskar Rainer <rainer.oskar@gmail.com>
2020-10-28 11:50:49 +0100

    feat: add default folder structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
 import { NextSeo } from "next-seo";
-import Header from "@src/components/Header";
+import Header from "@components/Header";
 
 const Index: NextPage = () => {
   return (
