5b2c7a4a889b1112eba115806980be636b1215f7
Oskar Rainer <rainer.oskar@gmail.com>
2020-11-07 12:31:38 +0100

    fix: next-seo opengraph tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,19 @@
 import NextHead from "next/head";
 import { AppProps } from "next/app";
-import { DefaultSeo, DefaultSeoProps } from "next-seo";
+import { DefaultSeo } from "next-seo";
 import { GlobalStyles } from "@styles/GlobalStyles";
-
-const defaultSeo: DefaultSeoProps = {
-  title: "wild-next",
-  description: "wild-next boilerplate",
-};
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <DefaultSeo {...defaultSeo} />
+      <DefaultSeo
+        title="wild-next"
+        titleTemplate="%s | wild-next"
+        description="This is the wild-next boilerplate."
+        openGraph={{}} // FIXME: https://github.com/garmeeh/next-seo/issues/544
+      />
       <NextHead>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
