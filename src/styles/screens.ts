6a3299efabf3f2577eabeba27eda7558c194dc8a
Oskar Rainer <rainer.oskar@gmail.com>
2020-11-30 16:12:59 +0100

    refactor(prettier): apply prettier

--- src/styles/screens.ts
+++ src/styles/screens.ts
@@ -5,6 +5,7 @@ import { theme } from "twin.macro";
  * @see https://tailwindcss.com/docs/breakpoints
  */
 export type Screen = "sm" | "md" | "lg" | "xl";
+export const screens = theme<Record<Screen, string>>("screens");
 
 // The maximum value is calculated as the minimum of the next one less 0.02px.
 // @see https://www.w3.org/TR/mediaqueries-4/#mq-min-max
@@ -12,24 +13,19 @@ const getNextBpValue = (bp: string) => {
   return `${parseInt(bp) - 0.02}px`;
 };
 
-export const screens = theme<Record<Screen, string>>("screens");
-
 export const up = (bp: Screen) => {
   const screen = screens[bp];
-
   return `@media only screen and (min-width: ${screen})`;
 };
 
 export const down = (bp: Screen) => {
   const screen = getNextBpValue(screens[bp]);
-
   return `@media only screen and (max-width: ${screen})`;
 };
 
 export const between = (bpMin: Screen, bpMax: Screen) => {
   const screenMin = screens[bpMin];
   const screenMax = getNextBpValue(screens[bpMax]);
-
   return `@media only screen and (min-width: ${screenMin}) and (max-width: ${screenMax})`;
 };
 
@@ -37,6 +33,5 @@ export const only = (bp: Screen) => {
   const screenKeys = Object.keys(screens) as Screen[];
   const currentKeyIndex = screenKeys.indexOf(bp);
   const nextBp = screenKeys[currentKeyIndex + 1];
-
   return nextBp ? between(bp, nextBp) : up(bp);
 };
