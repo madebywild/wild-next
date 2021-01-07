eaf8fec234960025d102748b62cecdccd639c1dc
Oskar <33655345+r0skar@users.noreply.github.com>
2021-01-07 10:36:03 +0100

    Add "useMediaQuery" hook (#41)
    * feat: add useMediaQuery hook
    
    * refactor: use simpler function arguments
    
    * feat: remove null as a default value

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { isClient } from "~/utils/common";
+
+const getMatch = (query: string) => {
+  return window.matchMedia(query);
+};
+
+const parseQueryString = (query: string) => {
+  return query.replaceAll("@media only screen and", "").trim();
+};
+
+export const useMediaQuery = (query: string, defaultState = false) => {
+  const parseAndMatch = (s: string) => getMatch(parseQueryString(s));
+  const [state, setState] = useState(isClient ? () => parseAndMatch(query).matches : defaultState);
+
+  useEffect(() => {
+    let mounted = true;
+    const mql = parseAndMatch(query);
+
+    const onChange = () => {
+      if (!mounted) return;
+      setState(!!mql.matches);
+    };
+
+    mql.addEventListener("change", onChange);
+    setState(mql.matches);
+
+    return () => {
+      mounted = false;
+      mql.removeEventListener("change", onChange);
+    };
+  }, [query]);
+
+  return state;
+};
