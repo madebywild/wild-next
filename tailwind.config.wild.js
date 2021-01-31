34495b9d4401ab74a9280b197e6dbc2f1d47afae
Oskar Rainer <rainer.oskar@gmail.com>
2021-01-31 14:12:23 +0100

    refactor(tailwind): cleanup TW config base

--- tailwind.config.wild.js
+++ tailwind.config.wild.js
@@ -3,30 +3,30 @@ const aspectRatioPlugin = require("@tailwindcss/aspect-ratio");
 
 const BASE_FONT_SIZE_PX = 10;
 
+const noop = (val) => val;
 const unitToPx = (val) => `${val}px`;
 const unitToRem = (val) => `${val}rem`;
 const pxToRem = (val) => val / BASE_FONT_SIZE_PX;
 const pxUnitToRem = (val) => unitToRem(pxToRem(val));
 
-const createScale = ({ min = 0, max = 100, steps = 1, valFM, keyFM }) => {
+const createScale = ({ min = 0, max = 100, steps = 1, formatVal = noop, formatKey = noop }) => {
   const limit = Math.round((max - min) / steps);
   const scale = [...new Array(limit + 1)].map((_, i) => min + i * steps);
 
   return scale.reduce((prev, curr) => {
-    const key = keyFM ? keyFM(curr) : curr;
-    const val = valFM && curr !== 0 ? valFM(curr) : curr;
-
-    return { ...prev, [String(key)]: val };
+    const key = String(formatKey(curr));
+    const val = curr === 0 ? curr : formatVal(curr);
+    return { ...prev, [key]: val };
   }, {});
 };
 
 const spacing = {
-  ...createScale({ max: 32, steps: 1, valFM: pxUnitToRem }),
-  ...createScale({ min: 32, max: 64, steps: 2, valFM: pxUnitToRem }),
-  ...createScale({ min: 68, max: 128, steps: 4, valFM: pxUnitToRem }),
-  ...createScale({ min: 136, max: 256, steps: 8, valFM: pxUnitToRem }),
-  ...createScale({ min: 272, max: 512, steps: 16, valFM: pxUnitToRem }),
-  ...createScale({ min: 544, max: 1024, steps: 32, valFM: pxUnitToRem }),
+  ...createScale({ max: 32, steps: 1, formatVal: pxUnitToRem }),
+  ...createScale({ min: 32, max: 64, steps: 2, formatVal: pxUnitToRem }),
+  ...createScale({ min: 68, max: 128, steps: 4, formatVal: pxUnitToRem }),
+  ...createScale({ min: 136, max: 256, steps: 8, formatVal: pxUnitToRem }),
+  ...createScale({ min: 272, max: 512, steps: 16, formatVal: pxUnitToRem }),
+  ...createScale({ min: 544, max: 1024, steps: 32, formatVal: pxUnitToRem }),
 };
 
 module.exports = {
