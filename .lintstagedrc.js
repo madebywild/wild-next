bba5348d3c2c626e5c8b262861c2ce0b96d2b52e
Oskar Rainer <rainer.oskar@gmail.com>
2021-05-07 16:00:29 +0200

    chore(lint-staged): minor update of targets

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
+// Run as function to fix tsc config path
+// https://github.com/okonet/lint-staged/issues/829#issuecomment-618649288
 module.exports = {
-  // Run as function to fix tsc config path
-  // https://github.com/okonet/lint-staged/issues/829#issuecomment-618649288
   "src/**/*.{ts,tsx}": () => ["npm run typecheck"],
-  "src/**/*.{js,jsx,ts,tsx}": ["npm run lint:fix", "npm run format"],
-  "src/**/*.{json,md,css}": ["npm run format"],
+  "src/**/*.{js,jsx,ts,tsx}": () => ["npm run lint:fix", "npm run format"],
+  "src/**/*.{json,md,css,scss}": () => ["npm run format"],
 };
