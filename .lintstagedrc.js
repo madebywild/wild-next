1d86e5c234c3cf99bca5692c74c1c55faacd64f5
Oskar Rainer <rainer.oskar@gmail.com>
2021-05-10 12:30:50 +0200

    fix(lint-staged): only run commands on staged files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
-// Run as function to fix tsc config path
-// https://github.com/okonet/lint-staged/issues/829#issuecomment-618649288
 module.exports = {
-  "src/**/*.{ts,tsx}": () => ["npm run typecheck"],
-  "src/**/*.{js,jsx,ts,tsx}": () => ["npm run lint:fix", "npm run format"],
-  "src/**/*.{json,md,css,scss}": () => ["npm run format"],
+  // Run as function to fix tsc config path
+  // https://github.com/okonet/lint-staged/issues/829#issuecomment-618649288
+  "src/**/*.{ts,tsx}": () => ["tsc --noEmit"],
+  "src/**/*.{json,md,css,scss}": ["prettier --write"],
+  "src/**/*.{js,jsx,ts,tsx}": ["eslint --color --fix", "prettier --write"],
 };
