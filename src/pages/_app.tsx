5ee7271b83b54a39aff33ec7c82f29838ff7e6d8
Oskar Rainer <rainer.oskar@gmail.com>
2022-03-25 16:53:38 +0100

    feat: update favicons & basePath support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import NextHead from "next/head";
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { DefaultSeo, DefaultSeoProps } from "next-seo";
 import { GlobalStyles } from "~/styles/GlobalStyles";
 
@@ -19,19 +20,27 @@ const defaultSeo: DefaultSeoProps = {
 };
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const { basePath } = useRouter();
+
   return (
     <>
       <DefaultSeo {...defaultSeo} />
       <NextHead>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
-        <link rel="manifest" href="/site.webmanifest" crossOrigin="use-credentials" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff" />
-        <link rel="preload" href="/fonts/Inter-Regular.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" key="viewport" />
+        <link rel="apple-touch-icon" sizes="180x180" href={`${basePath}/apple-touch-icon.png`} key="apple" />
+        <link rel="icon" type="image/png" sizes="32x32" href={`${basePath}/favicon-32x32.png`} key="icon32" />
+        <link rel="icon" type="image/png" sizes="16x16" href={`${basePath}/favicon-16x16.png`} key="icon16" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" key="mask" />
+        <meta name="msapplication-TileColor" content="#000000" key="tile" />
+        <meta name="theme-color" content="#ffffff" key="theme" />
+        <link rel="manifest" href={`${basePath}/site.webmanifest`} crossOrigin="use-credentials" />
+        <link
+          rel="preload"
+          href={`${basePath}/fonts/Inter-Regular.otf`}
+          as="font"
+          type="font/otf"
+          crossOrigin="anonymous"
+        />
       </NextHead>
       <GlobalStyles />
       <Component {...pageProps} />
