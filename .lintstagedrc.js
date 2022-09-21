34bcf51c5aead58bea2abb002b9580d417673727
Oskar Rainer <rainer.oskar@gmail.com>
2022-09-21 14:57:14 +0200

    build: improve lint-staged setup

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Run as function to fix tsc config path
   // https://github.com/okonet/lint-staged/issues/829#issuecomment-618649288
-  "src/**/*.{ts,tsx}": () => ["tsc --noEmit"],
-  "src/**/*.{json,md,css,scss}": ["prettier --write"],
-  "src/**/*.{js,jsx,ts,tsx}": ["eslint --color --fix", "prettier --write"],
+  "*.{ts,tsx}": () => ["tsc --noEmit"],
+  "*.{json,md,css,scss}": ["prettier --write"],
+  "*.{js,jsx,ts,tsx}": ["eslint --quiet --fix"],
 };
