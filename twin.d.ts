96cae846fe027b7496aaf4c512632cceff2fedf1
Oskar Rainer <rainer.oskar@gmail.com>
2022-09-22 10:12:46 +0200

    fix: use type imports when appropriate

--- twin.d.ts
+++ twin.d.ts
@@ -1,5 +1,5 @@
 import "twin.macro";
-import type styledImport, { type CSSProp, css as cssImport } from "styled-components";
+import styledImport, { type CSSProp, css as cssImport } from "styled-components";
 
 declare module "twin.macro" {
   const styled: typeof styledImport;
