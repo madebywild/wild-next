c66b03fdceffd5021a95088332067339a8e172e3
Oskar Rainer <rainer.oskar@gmail.com>
2023-12-04 11:57:57 +0100

    fix: rm lang

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 const Document = () => {
   return (
-    <Html lang="en">
+    <Html>
       <Head>
         <Script src="/unsupported-browser/browser-check.js" strategy="beforeInteractive" />
       </Head>
