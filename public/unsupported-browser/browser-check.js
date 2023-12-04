c66819ccfe6a1be95cdda8cb7a6bddfd22576ef7
Oskar Rainer <rainer.oskar@gmail.com>
2023-12-04 11:52:53 +0100

    feat: unsupported browser fallback

---
+++ public/unsupported-browser/browser-check.js
@@ -0,0 +1,12 @@
+function browserCheck() {
+  // https://github.com/w3c/csswg-drafts/issues/3559#issuecomment-1758459996
+  const supportsFlexGap = typeof CSS !== "undefined" ? CSS.supports("inset: 0") : false;
+
+  if (!supportsFlexGap) {
+    window.location.href = "/unsupported-browser/index.html";
+  }
+}
+
+if (typeof window !== "undefined") {
+  browserCheck();
+}
