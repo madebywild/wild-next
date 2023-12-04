f28639bd42d56245090341b7ee457739e19d54ce
Oskar Rainer <rainer.oskar@gmail.com>
2023-12-04 12:24:05 +0100

    fix: disable linter

--- public/unsupported-browser/browser-check.js
+++ public/unsupported-browser/browser-check.js
@@ -1,6 +1,8 @@
+/* eslint-disable */
+
 function checkFeatures() {
   // https://github.com/w3c/csswg-drafts/issues/3559#issuecomment-1758459996
-  const supportsFlexGap = typeof CSS !== "undefined" ? CSS.supports("inset: 0") : false;
+  var supportsFlexGap = typeof CSS !== "undefined" ? CSS.supports("inset: 0") : false;
 
   if (!supportsFlexGap) {
     window.location.href = "/unsupported-browser/index.html";
