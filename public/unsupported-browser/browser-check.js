2b54d3ebea81883eb98dcb9924ba5c359e31653d
Oskar Rainer <rainer.oskar@gmail.com>
2023-12-04 12:23:15 +0100

    chore: pr review

--- public/unsupported-browser/browser-check.js
+++ public/unsupported-browser/browser-check.js
@@ -1,4 +1,4 @@
-function browserCheck() {
+function checkFeatures() {
   // https://github.com/w3c/csswg-drafts/issues/3559#issuecomment-1758459996
   const supportsFlexGap = typeof CSS !== "undefined" ? CSS.supports("inset: 0") : false;
 
@@ -8,5 +8,5 @@ function browserCheck() {
 }
 
 if (typeof window !== "undefined") {
-  browserCheck();
+  checkFeatures();
 }
